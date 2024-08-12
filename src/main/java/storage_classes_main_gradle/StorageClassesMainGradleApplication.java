f7285854c509f406b48ff4626128bcd1914eed99
alibek1104 <nurmahanovalibek@gmail.com>
2024-08-12 23:37:47 +0600

    updated

---
+++ src/main/java/storage_classes_main_gradle/StorageClassesMainGradleApplication.java
@@ -0,0 +1,13 @@
+package storage_classes_main_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageClassesMainGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageClassesMainGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/storage_classes_main_gradle/StorageClassesMainGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package storage_classes_main_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageClassesMainGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
